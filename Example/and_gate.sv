e8909ff5ffb4f09e9c9a777d17ed9c3e42deea21
SwamarpanRoy <itsroyy.tech04@gmail.com>
2026-02-04 18:01:26 +0530

    Create and_gate.sv

---
+++ Example/and_gate.sv
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+//File       : and_gate.sv
+//Author     : Swamarpan Roy/1BM23EC305
+//Created    : 2026-01-20
+//Module     : and_gate
+//Project    : SystemVerilog and Verification (23EC6PE2SV),
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description: 2-input AND gate used for basic functional coverage example.
+//------------------------------------------------------------------------------
+
+module and_gate(input logic a,b, output logic y);
+  
+  always@(*)
+    y = a&b;
+endmodule
